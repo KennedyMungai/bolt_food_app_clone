8f7aea4fd76c9320898f63a31d463a7e2f216b92
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-14 11:19:42 +0300

    Made some simple changes

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native';
 import React from 'react';
+import { SafeAreaView, View } from 'react-native';
 
 const TabHomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView className={styles.container}>
+      <View className={styles.header}>
+        <View className={styles.addressContainer}></View>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default TabHomeScreen;
+
+const styles = {
+  container: 'flex-1 p-4 mt-12 bg-white',
+  header: '',
+  addressContainer: '',
+};
