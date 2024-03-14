1f396470a0bbf185122f1e6f294b167d554c0a33
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-14 11:56:04 +0300

    Made some changes to the component

--- components/market-card.tsx
+++ components/market-card.tsx
@@ -1,3 +1,4 @@
+import { FontAwesome } from '@expo/vector-icons';
 import React from 'react';
 import { Image, Pressable, Text, View } from 'react-native';
 import { dummyRestaurantsData } from '~/assets/data/restaurantsData';
@@ -15,6 +16,14 @@ const MarketCard = () => {
           <Text className={styles.overlayText}>{dummyRestaurantsData[0].delivery} min</Text>
         </View>
       </View>
+      <View className={styles.textContainer}>
+        <Text className={styles.restaurantName}>{dummyRestaurantsData[0].name}</Text>
+        <View className={styles.ratingContainer}>
+          <FontAwesome name="star" size={24} />
+          <Text className={styles.ratingText}>{dummyRestaurantsData[0].rating}</Text>
+        </View>
+      </View>
+      <Text className={styles.priceText}>$ {dummyRestaurantsData[0].price}</Text>
     </Pressable>
   );
 };
@@ -26,4 +35,9 @@ const styles = {
   cardImage: 'w-full h-[180px] rounded-md',
   overlay: 'absolute bg-white rounded-sm bottom-2 right-2',
   overlayText: 'text-sm font-semibold py-1 px-2',
+  textContainer: 'flex flex-row items-center justify-between py-2',
+  restaurantName: 'font-bold text-2xl text-neutral-600',
+  ratingContainer: 'flex flex-row gap-5',
+  ratingText: 'text-2xl text-neutral-500',
+  priceText: 'font-semibold text-[1.5rem] text-orange-500',
 };
