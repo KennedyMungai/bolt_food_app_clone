59124438024b14d2e75b5a81fa573e00df8739a6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-14 11:01:44 +0300

    Updated the config for the tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -17,12 +17,21 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
+        name="search"
+        options={{
+          title: 'Search',
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarIcon: ({ color }) => <Ionicons name="search" color={color} size={24} />,
+        }}
+      />
+      <Tabs.Screen
         name="orders"
         options={{
           title: 'Orders',
           tabBarShowLabel: false,
           headerShown: false,
-          tabBarIcon: ({ color }) => <Ionicons name="clipboard-sharp" color={color} size={24} />,
+          tabBarIcon: ({ color }) => <Ionicons name="receipt-outline" color={color} size={24} />,
         }}
       />
       <Tabs.Screen
@@ -34,15 +43,6 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => (
             <Ionicons name="person-circle-sharp" color={color} size={24} />
           ),
-        }}
-      />
-      <Tabs.Screen
-        name="search"
-        options={{
-          title: 'Search',
-          tabBarShowLabel: false,
-          headerShown: false,
-          tabBarIcon: ({ color }) => <Ionicons name="search" color={color} size={24} />,
         }}
       />
     </Tabs>
