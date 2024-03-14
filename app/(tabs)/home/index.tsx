113c957fd71093643d3ec34cabf1f1cf713e5a88
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-14 11:24:27 +0300

    Added some markup for to the Home page

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,11 +1,15 @@
+import { FontAwesome6, MaterialCommunityIcons } from '@expo/vector-icons';
 import React from 'react';
-import { SafeAreaView, View } from 'react-native';
+import { SafeAreaView, Text, View } from 'react-native';
 
 const TabHomeScreen = () => {
   return (
     <SafeAreaView className={styles.container}>
       <View className={styles.header}>
-        <View className={styles.addressContainer}></View>
+        <View className={styles.addressContainer}>
+          <MaterialCommunityIcons name="map-marker-outline" size={28} />
+          <Text className={styles.addressText}>Your Address Here</Text>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -15,6 +19,7 @@ export default TabHomeScreen;
 
 const styles = {
   container: 'flex-1 p-4 mt-12 bg-white',
-  header: '',
-  addressContainer: '',
+  header: 'flex-row justify-between',
+  addressContainer: 'flex-row items-center',
+  addressText: 'ml-2',
 };
