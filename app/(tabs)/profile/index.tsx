3e8cca484e9f2818bca27467b5c5c1cdb29d01d6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-14 10:40:38 +0300

    Created the profile tab

---
+++ app/(tabs)/profile/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const TabsProfilePage = () => {
+  return (
+    <View>
+      <Text>TabsProfilePage</Text>
+    </View>
+  );
+};
+
+export default TabsProfilePage;
