8b3fb59397a878dc0accb6440d6ea46879e6264d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-14 11:36:55 +0300

    Rendered out an image on the market card component

--- components/market-card.tsx
+++ components/market-card.tsx
@@ -1,12 +1,24 @@
-import { View, Text } from 'react-native';
 import React from 'react';
+import { Image, Pressable, View } from 'react-native';
+import { dummyRestaurantsData } from '~/assets/data/restaurantsData';
 
 const MarketCard = () => {
   return (
-    <View>
-      <Text>MarketCart</Text>
-    </View>
+    <Pressable className={styles.cardContainer}>
+      <View>
+        <Image
+          source={{ uri: dummyRestaurantsData[0].profileImage }}
+          className={styles.cardImage}
+          resizeMode="cover"
+        />
+      </View>
+    </Pressable>
   );
 };
 
 export default MarketCard;
+
+const styles = {
+  cardContainer: 'mt-5',
+  cardImage: 'w-full h-[180px] rounded-md',
+};
